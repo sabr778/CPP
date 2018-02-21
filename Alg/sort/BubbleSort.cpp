b74c48816ab4fd0f30d53a7526c7c723a2fc0ef8
unknown <saberyu778@gmail.com>
2018-02-21 11:37:21 -0800

    bubble sort optimization

--- Alg/sort/BubbleSort.cpp
+++ Alg/sort/BubbleSort.cpp
@@ -9,16 +9,18 @@ public:
     vector<int> solve(vector<int> array)
     {
         if (array.empty()) return *new vector<int>();
-        for (int count = 0; count < array.size();)
+        for (int count = 1; count < array.size(); count++)
         {
-            count ++;
-            for (vector<int>::iterator j = array.begin() ; j != array.end()-count; ++j)
+            bool swapped = false;
+            for (auto j = array.begin() ; j < array.end()-count; ++j)
             {
                 if (*(j) > *(j+1))
                 {
                     iter_swap(j, j+1);
+                    swapped = true;
                 }
             }
+            if (!swapped) break;
         }
         return array;
     }
@@ -26,7 +28,7 @@ public:
 
 int main() {
     int arr[] = {5,7,10,9,8,1,3,6,4,2};
-    Solution *sol = new Solution;
+    auto *sol = new Solution;
     vector<int> array(arr, arr + sizeof(arr)/sizeof(int));
     array = sol->solve(array);
     for (int i : array)
