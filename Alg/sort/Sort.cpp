d3bdef55c5fd2329b0947b608b607edb78619ea6
sabr778 <saberyu778@gmail.com>
2018-02-25 20:35:40 -0800

    merge sort. Debug for a while... finally - 1. plain void method is fine. 2. iterator

--- Alg/sort/Sort.cpp
+++ Alg/sort/Sort.cpp
@@ -5,7 +5,44 @@ using namespace std;
 
 class Solution
 {
+private:
+    void merge(vector<int> array, vector<int>::iterator left, vector<int>::iterator mid, vector<int>::iterator right)
+    {
+        vector<int> array1 = vector<int>(left, mid);
+        vector<int> array2 = vector<int>(mid, right);
+        auto i = array1.begin();
+        auto j = array2.begin();
+        while (i < array1.end() && j < array2.end())
+        {
+            if (*i < *j)
+                *(left++) = *(i++);
+            else *(left++) = *(j++);
+        }
+        while (i < array1.end())
+            *(left++) = *(i++);
+        while (j < array2.end())
+            *(left++) = *(j++);
+    }
+
+    void mergeSortHelper(vector<int> array, vector<int>::iterator left, vector<int>::iterator right)
+    {
+        if (left < right-1)
+        {
+            auto mid = left + (right - left) / 2;
+            mergeSortHelper(array, left, mid);
+            mergeSortHelper(array, mid, right);
+            merge(array, left, mid, right);
+        }
+    }
+
 public:
+    vector<int> mergeSort(vector<int> array)
+    {
+        if (array.empty()) return *new vector<int>;
+        mergeSortHelper(array, array.begin(), array.end());
+        return array;
+    }
+
     vector<int> selectionSort(vector<int> array)
     {
         if (array.empty()) return *new vector<int>;
@@ -23,7 +60,6 @@ public:
         return array;
     }
 
-public:
     vector<int> insertionSort(vector<int> array)
     {
         if (array.empty()) return *new vector<int>();
@@ -31,7 +67,7 @@ public:
         {
             int key = array[count];
             int j = count-1;
-            while (j >=0 && array[j] >= key) 
+            while (j >=0 && array[j] >= key)
             {
                 array[j+1] = array[j];
                 j--;
@@ -41,7 +77,6 @@ public:
         return array;
     }
 
-public:
     vector<int> bubbleSort(vector<int> array)
     {
         if (array.empty()) return *new vector<int>();
@@ -60,14 +95,14 @@ public:
         }
         return array;
     }
-    
+
 };
 
 int main() {
     int arr[] = {5,7,10,9,8,1,3,6,4,2};
     auto *sol = new Solution;
     vector<int> array(arr, arr + sizeof(arr)/sizeof(int));
-    array = sol->solve(array);
+    array = sol -> mergeSort(array);
     for (int i : array)
     {
         cout << i << ",";
