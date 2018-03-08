cafdd26dab7f468af7be9e0e1b05f42e9b336037
unknown <saberyu778@gmail.com>
2018-03-08 14:48:07 -0800

    binary search find closest

--- Alg/BinarySearch/BinarySearch.cpp
+++ Alg/BinarySearch/BinarySearch.cpp
@@ -3,6 +3,7 @@ class BinarySearch
 public:
 	int classic(vector<int> input, int target) 
 	{
+        //If found return the index of it, otherwise return -1.
     	if (input.empty()) return -1;
     	auto left = input.begin();
     	auto right = input.end()-1;
@@ -17,8 +18,9 @@ public:
   	}
 
 
-  	int firstOccur(vector<int> array, int target) 
+  	int firstOccur(vector<int> input, int target) 
   	{
+        //If found duplicated return the index of the firstly occurred, otherwise return -1.
   		if (input.empty()) return -1;
     	auto left = input.begin();
     	auto right = input.end()-1;
@@ -30,5 +32,36 @@ public:
     	}
     	return *left == target ? left - input.begin() : (*right == target ? right - input.begin() : -1);
   	}
+
+    int lastOccur(vector<int> input, int target) 
+    {
+        //If found duplicated return the index of the lastly occurred, otherwise return -1.
+        if (input.empty()) return -1;
+        auto left = input.begin();
+        auto right = input.end()-1;
+        while (left < right -1)
+        {
+            auto mid = left + (right - left)/2;
+            if (*mid <= target) left = mid;
+            else right = mid;
+        }
+        return *right == target ? right - input.begin() : ( *left == target ? left - input.begin() : -1);
+    }
+
+    int findClosest(vector<int> input, int target) 
+    {
+        //find the index of the number closest to the target. If empty vector return -1.
+        if (input.empty()) return -1;
+        auto left = input.begin();
+        auto right = input.end()-1;
+        while (left < right -1)
+        {
+            auto mid = left + (right - left)/2;
+            if (*mid == target) return mid - input.begin();
+            if (*mid < target) left = mid;
+            else right = mid;
+        }
+        return abs(*left - target) <= abs(*right - target) ? left - input.begin() : right - input.begin();
+    }
 
 };
